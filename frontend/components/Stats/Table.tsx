2810f18a35f665ae1754b06276bcd01aa1248b2b
Oleksandr Petryk <sachapos50@gmail.com>
2022-11-19 19:36:21 +0200

    feat: add link to open transaction in etherscan.io

--- frontend/components/Stats/Table.tsx
+++ frontend/components/Stats/Table.tsx
@@ -33,7 +33,15 @@ export const Table: FC<{ className?: string }> = ({ className }) => {
               return (
                 <tr key={transaction.hash}>
                   <td className='truncate'>{ parseInt(transaction.blockNumber, 16) }</td>
-                  <td className='truncate'>{ transaction.hash }</td>
+                  <td className='truncate'>
+                    <a
+                      href={'https://etherscan.io/tx/' + transaction.hash}
+                      target="_blank"
+                      className='text-blue-500 underline'
+                    >
+                      { transaction.hash }
+                    </a>
+                    </td>
                   <td className='truncate'>{ transaction.from }</td>
                   <td className='truncate'>{ transaction.to }</td>
                   <td className='truncate'>{ parseInt(transactions.latestBlockNumber, 16) - parseInt(transaction.blockNumber, 16) }</td>
